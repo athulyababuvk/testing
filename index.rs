ffcedb1da87c76f8b46e358bfccedbd0502970b3
athulya <athulyababu.vk@surya-soft.com>
2020-04-07 15:28:58 +0530

    second commit

--- index.rs
+++ index.rs
@@ -1,4 +1,11 @@
 fn main(){
     println!("Hello world !");
+    addfn(34,92);
+}
+
+//function for adding two numbers 
+pub fn addfn(a:i32, b:i32){
+    let c = a + b ;
+    println!("Sum of `a` and `b` is {}",c);
 }
 
